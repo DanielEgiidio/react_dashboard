8cee91988e3e9b5b7aa45c7cf20bee11b74e87ff
DanielEgidio <danielegidio@si.fiponline.edu.br>
2022-04-04 09:50:36 -0300

    Implementação e estilização dos links Sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,92 +1,87 @@
-import "./Sidebar.css";
-import {
-  LineStyle,
-  Timeline,
-  TrendingUp,
-  PermIdentity,
-  Storefront,
-  AttachMoney,
-  BarChart,
-  MailOutline,
-  DynamicFeed,
-  ChatBubbleOutline,
-  WorkOutline,
-  Report,
-} from "@material-ui/icons";
+import "./Sidebar.scss";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
+import LocalShippingIcon from "@mui/icons-material/LocalShipping";
+import CreditCardIcon from "@mui/icons-material/CreditCard";
+import StoreIcon from "@mui/icons-material/Store";
+import InsertChartIcon from "@mui/icons-material/InsertChart";
+import SettingsApplicationsIcon from "@mui/icons-material/SettingsApplications";
+import ExitToAppIcon from "@mui/icons-material/ExitToApp";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import SettingsSystemDaydreamOutlinedIcon from "@mui/icons-material/SettingsSystemDaydreamOutlined";
+import PsychologyOutlinedIcon from "@mui/icons-material/PsychologyOutlined";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
 
-export default function Sidebar() {
+const Sidebar = () => {
   return (
     <div className="sidebar">
-      <div className="sidebarWrapper">
-        <div className="sidebarMenu">
-          <h3 className="sidebarTitle">Dashboard</h3>
-          <ul className="sidebarList">
-            <li className="sidebarListItem active">
-              <LineStyle className="sidebarIcon" />
-              Home
-            </li>
-            <li className="sidebarListItem ">
-              <Timeline className="sidebarIcon" />
-              Análise
-            </li>
-            <li className="sidebarListItem">
-              <TrendingUp className="sidebarIcon" />
-              Vendas
-            </li>
-          </ul>
-        </div>
-        <div className="sidebarMenu">
-          <h3 className="sidebarTitle">Menu Rápido</h3>
-          <ul className="sidebarList">
-            <li className="sidebarListItem active">
-              <PermIdentity className="sidebarIcon" />
-              Usuários
-            </li>
-            <li className="sidebarListItem ">
-              <Storefront className="sidebarIcon" />
-              Produtos
-            </li>
-            <li className="sidebarListItem">
-              <TrendingUp className="sidebarIcon" />
-              Transações
-            </li>
-          </ul>
-        </div>
-        <div className="sidebarMenu">
-          <h3 className="sidebarTitle">Notificações</h3>
-          <ul className="sidebarList">
-            <li className="sidebarListItem active">
-              <MailOutline className="sidebarIcon" />
-              Email
-            </li>
-            <li className="sidebarListItem ">
-              <DynamicFeed className="sidebarIcon" />
-              Feedback
-            </li>
-            <li className="sidebarListItem">
-              <ChatBubbleOutline className="sidebarIcon" />
-              Mensagens
-            </li>
-          </ul>
-        </div>
-        <div className="sidebarMenu">
-          <h3 className="sidebarTitle">Staff</h3>
-          <ul className="sidebarList">
-            <li className="sidebarListItem active">
-              <WorkOutline className="sidebarIcon" />
-              Administração
-            </li>
-            <li className="sidebarListItem ">
-              <Timeline className="sidebarIcon" />
-              Análise
-            </li>
-            <li className="sidebarListItem">
-              <Report className="sidebarIcon" />
-              Relatórios
-            </li>
-          </ul>
-        </div>
+      <div className="top">
+        <span className="logo">Admin Dashboard</span>
+      </div>
+      <hr />
+      <div className="center">
+        <ul>
+          <p className="title">PRINCIPAL</p>
+          <li>
+            <DashboardIcon className="icon" />
+            <span>DashBoard</span>
+          </li>
+          <p className="title">lISTA</p>
+          <li>
+            <PersonOutlineIcon className="icon" />
+            <span>Usuários</span>
+          </li>
+          <li>
+            <StoreIcon className="icon" />
+            <span>Produtos</span>
+          </li>
+          <li>
+            <CreditCardIcon className="icon" />
+            <span>Pedidos</span>
+          </li>
+          <li>
+            <LocalShippingIcon className="icon" />
+            <span>Delivery</span>
+          </li>
+          <p className="title">lINKS</p>
+          <li>
+            <InsertChartIcon className="icon" />
+            <span>Status</span>
+          </li>
+          <li>
+            <NotificationsNoneIcon className="icon" />
+            <span>Notificações</span>
+          </li>
+          <p className="title">SERVIÇOS</p>
+          <li>
+            <SettingsSystemDaydreamOutlinedIcon className="icon" />
+            <span>System Health</span>
+          </li>
+          <li>
+            <PsychologyOutlinedIcon className="icon" />
+            <span>Logs</span>
+          </li>
+          <li>
+            <SettingsApplicationsIcon className="icon" />
+            <span>Configurações</span>
+          </li>
+          <p className="title">USUÁRIO</p>
+          <li>
+            <AccountCircleOutlinedIcon className="icon" />
+            <span>Perfil</span>
+          </li>
+          <li>
+            <ExitToAppIcon className="icon" />
+            <span>Logout</span>
+          </li>
+        </ul>
+      </div>
+      <div className="bottom">
+        <div className="colorOptions"></div>
+        <div className="colorOptions"></div>
       </div>
     </div>
   );
-}
+};
+
+export default Sidebar;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 import "./Home.scss";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Navbar from "../../components/navbar/Navbar";
 
 const Home = () => {
-  return <div className="home">DarshBoard Admin</div>;
+  return (
+    <div className="home">
+      <Navbar />
+      <Sidebar />
+      <div className="homeContainer">container</div>
+    </div>
+  );
 };
 
 export default Home;
